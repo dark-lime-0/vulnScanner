d5071734eb3276c33e03dcc5b45fde0c53ea1e11
youssef <Dark88Lime@gmail.com>
2024-01-22 18:39:42 +0100

    v0.0.1

---
+++ vulnScanner.py
@@ -0,0 +1,34 @@
+import requests
+
+logo='''
+        __        __   _    ____  _ _                       
+        \ \      / /__| |__/ ___|(_) |_ ___
+         \ \ /\ / / _ \ '_ \___ \| | __/ _ \ 
+          \ V  V /  __/ |_) |__) | | ||  __/
+__     __  \_/\_/ \___|_.__/____/|_|\__\___| _ _ _
+\ \   / /   _| |_ __   ___ _ __ __ _| |__ (_) (_) |_ _   _ 
+ \ \ / / | | | | '_ \ / _ \ '__/ _` | '_ \| | | | __| | | |
+  \ V /| |_| | | | | |  __/ | | (_| | |_) | | | | |_| |_| |
+   \_/  \__,_|_|_| |_|\___|_|  \__,_|_.__/|_|_|_|\__|\__, |
+         ____                                        |___/  
+        / ___|  ___ __ _ _ __  _ __   ___ _ __
+        \___ \ / __/ _` | '_ \| '_ \ / _ \ '__|
+         ___) | (_| (_| | | | | | | |  __/ |
+        |____/ \___\__,_|_| |_|_| |_|\___|_|
+        
+v0.0.1 by Youssef | dark-lime-0 
+'''
+print(logo)
+
+domain=input("Enter Your Website Link : ")
+wb_headers=requests.get(domain).headers
+headers=['Strict-Transport-Security','Content-Security-Policy','X-Frame-Options','X-Content-Type-Options','Referrer-Policy','Permissions-Policy']
+print("\n")
+b=0
+for a in headers:
+    if a not in wb_headers:
+        print(a + " :: doesn't exist")
+        b+=1
+
+if(b !=0):print("\nYour Website May Vulnerable to *Clickjacking* Vulnerability !! ")
+else:print("\nAll Great !!")
